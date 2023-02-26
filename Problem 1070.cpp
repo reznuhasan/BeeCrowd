233e7b6a12f4c3a8b4ffb0c108f8d7b16c63a894
reznuhasan <reznuhasan31@gmail.com>
2023-02-26 16:54:10 +0600

    add problem 1070

---
+++ Problem 1070.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    int res=0;
+    while(res<6){
+        if(x%2==1)
+        {
+            cout<<x<<endl;
+            res++;
+        }
+        x++;
+
+    }
+
+
+
+}
+
