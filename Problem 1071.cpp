ab1f58c827e7bed3465679cf1ceac1ccbe0bc81b
reznuhasan <reznuhasan31@gmail.com>
2023-02-26 19:51:06 +0600

    problem 1071

---
+++ Problem 1071.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    int min=0;
+    int max=0;
+    if(x>y){
+        min=y;
+        max=x;
+    }else{
+        min=y;
+        max=x;
+    }
+    int sum=0;
+    for(int i=min+1;i<max;i++){
+        if(i%2!=0){
+            cout<<i<<endl;
+            sum=sum+i;
+        }
+    }
+    cout<<sum<<endl;
+
+}
