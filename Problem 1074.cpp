8a0fe8a540a74126448ccf88f5632cc5cd4df185
reznuhasan <reznuhasan31@gmail.com>
2023-02-26 20:29:39 +0600

    add problem 1074

--- Problem 1074.cpp
+++ Problem 1074.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n<10000){
+        for(int i=1;i<=n;i++){
+            int num;
+            cin>>num;
+            if(num==0){
+                cout<<"NULL";
+            }else{
+                if(num%2==0){
+                    cout<<"EVEN ";
+                }else{
+                    cout<<"ODD ";
+                }
+                if(num>0){
+                    cout<<"POSITIVE";
+                }else{
+                    cout<<"NEGATIVE";
+                }
+            }
+            cout<<endl;
+        }
+    }
+}
 
