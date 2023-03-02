4e7b59628969350074714bc1b8f9cc435a9e9c86
reznuhasan <reznuhasan31@gmail.com>
2023-03-02 15:45:16 +0600

    add Problem 1098

---
+++ Problem 1098.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(float i=0;i<2.2;i+=0.2){
+        for(float j=1;j<=3;j++){
+            cout<<"I="<<i<<" J="<<j+i<<endl;
+        }
+    }
+}
