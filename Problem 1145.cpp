3a18cabb35557454c61145e3198ca6e35a0c3ef9
reznuhasan <reznuhasan31@gmail.com>
2023-03-06 22:06:56 +0600

    add 1145 edit

--- Problem 1145.cpp
+++ Problem 1145.cpp
@@ -6,19 +6,13 @@ int main()
     int x,y;
     cin>>x>>y;
     int flag=1;
-    if((x>1 && x<20) && (y>x && y<100000))
-    {
-        for(int i=1; i<=y; i++)
-        {
+    for(int i=1;i<=y;i++){
+        if(flag==x){
+            cout<<i<<endl;
+            flag=1;
+        }else{
             cout<<i<<" ";
-            if(x>flag){
-                flag++;
-            }else{
-               flag=1;
-               cout<<endl;
-            }
-
-
+            flag++;
         }
     }
 
