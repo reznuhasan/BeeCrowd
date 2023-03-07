223afeb83173c3c8dd6dfa7e0dd9d0497051fd95
reznuhasan <reznuhasan31@gmail.com>
2023-03-07 21:26:20 +0600

    problem 1153 add

---
+++ Problem 1153.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int multi=1;
+    for(int i=1;i<=n;i++){
+        multi *=i;
+    }
+    cout<<multi<<endl;
+}
